abaa7f310a7b7a2879de2b17674b48da101ab22a
lucianoovicente-a11y <luciano.ovicente@gmail.com>
2026-06-25 10:54:38 -0300

    Add gradle-wrapper.jar, fix compileSdk 33, fix workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.streamflow.player"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.streamflow.player"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
