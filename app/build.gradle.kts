80a1543abc78adb8014404b08e162cf98b1a7f23
lucianoovicente-a11y <luciano.ovicente@gmail.com>
2026-06-25 11:18:14 -0300

    Fix compileSdk 34, install android-34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.streamflow.player"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.streamflow.player"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
