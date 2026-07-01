9d37a2f28ea30b7690448688114885793b9c4e7f
fjreidamoeda <148280932+fjreidamoeda@users.noreply.github.com>
2026-07-01 16:57:00 -0300

    restore original file - revert debug changes

--- app/src/main/java/com/streamflow/player/MainActivity.kt
+++ app/src/main/java/com/streamflow/player/MainActivity.kt
@@ -2,7 +2,6 @@ package com.streamflow.player
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,25 +10,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        try {
-            configManager = ConfigManager(this)
+        configManager = ConfigManager(this)
 
-            if (configManager.isConfigured) {
-                startActivity(Intent(this, MainMenuActivity::class.java))
-            } else {
-                startActivity(Intent(this, SetupActivity::class.java))
-            }
-
-            finish()
-        } catch (e: Exception) {
-            android.widget.TextView(this).apply {
-                text = "ERRO: ${e.message}\n\n${e.stackTraceToString()}"
-                textSize = 14f
-                setTextColor(-0x1)
-                setBackgroundColor(-0x1000000)
-                setHorizontallyScrolling(false)
-            }.let { setContentView(it) }
-            e.printStackTrace()
+        if (configManager.isConfigured) {
+            startActivity(Intent(this, MainMenuActivity::class.java))
+        } else {
+            startActivity(Intent(this, SetupActivity::class.java))
         }
+
+        finish()
     }
 }
+
